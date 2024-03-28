257fb95f8927ecb8b97df2897e53e6fbfe865b48
Amit <amit.yd525@gmail.com>
2024-03-29 02:39:52 +0530

    Application page almost done

--- src/pages/ApplicationsPage.jsx
+++ src/pages/ApplicationsPage.jsx
@@ -12,8 +12,8 @@ const ApplicationsPage = () => {
 
   console.log(userData);
   return (
-    <div className="m-8 border border-black">
-      <h1 className="text-3xl font-bold   text-gray-800">
+    <div className="m-8 sm:m-2">
+      <h1 className="text-3xl font-bold px-5 py-3  text-gray-800">
         Hello, {userData.fullName.toUpperCase()}!{" "}
       </h1>
       <section className="m-6 p-4 pb-20 rounded-xl border-gray-300 shadow-md">
@@ -27,7 +27,7 @@ const ApplicationsPage = () => {
         </p>
         <NavLink
           to="/profile"
-          className="mt-4 inline-block px-6 py-2 bg-green-500 text-white font-semibold rounded-md shadow-md hover:bg-green-600 transition duration-300"
+          className="mt-4 inline-block px-6 py-2 bg-green-500 text-white font-semibold rounded-md shadow-sm hover:bg-green-600 transition duration-300"
         >
           View Your Profile
         </NavLink>
@@ -40,22 +40,22 @@ const ApplicationsPage = () => {
           <button
             className={`mr-2 px-4 py-2  ${
               activeTab === "submitted"
-                ? "bg-blue-500 text-white font-semibold"
-                : "bg-gray-200 text-gray-700 font-semibold"
+                ? "bg-blue-500 text-white font-semibold "
+                : "bg-gray-100 text-gray-700 font-semibold hover:bg-gray-200 transition duration-100"
             }`}
             onClick={() => handleTabChange("submitted")}
           >
-            Submitted <span>(2)</span>
+            Submitted <span>(4)</span>
           </button>
           <button
             className={`px-4 py-2 ${
               activeTab === "archived"
                 ? "bg-blue-500 text-white font-semibold"
-                : "bg-gray-200 text-gray-700 font-semibold"
+                : "bg-gray-100 text-gray-700 font-semibold hover:bg-gray-200 transition duration-100"
             }`}
             onClick={() => handleTabChange("archived")}
           >
-            Archived <span>(2)</span>
+            Archived <span>(5)</span>
           </button>
         </div>
         <hr className="my-1 border-gray-400" /> {/* Horizontal line */}
@@ -63,13 +63,160 @@ const ApplicationsPage = () => {
         {activeTab === "submitted" && (
           <div>
             {/* Content for Submitted tab */}
-            <h2>Submitted Applications</h2>
+
+            <div className="grid lg:grid-cols-3 md:grid-cols-2">
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700  ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700 ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700 ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700 ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+            </div>
           </div>
         )}
         {activeTab === "archived" && (
           <div>
             {/* Content for Archived tab */}
-            <h2>Archived Applications</h2>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2">
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700 ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700 ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700 ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700 ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+              <div className="m-4 p-4 border border-gray-200 rounded-lg text-gray-700 ">
+                <h1 className="font-semibold text-xl">
+                  Teaching Assisstent Requirement
+                </h1>
+                <h3>Course Id : CSPC 302</h3>
+                <h3>Applied Date: March 29, 2024</h3>
+                <h3>
+                  Status:{" "}
+                  <span className="bg-slate-100  rounded-lg text-sm px-2 py-0.5 text-gray-700 font-mono font-semibold">
+                    pending
+                  </span>
+                </h3>
+                <button className="mt-10  text-blue-600 font-semibold text-sm hover:bg-slate-100 px-3 py-2 rounded-md">
+                  View application
+                </button>
+              </div>
+            </div>
           </div>
         )}
       </section>
