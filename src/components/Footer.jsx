3d91a164fa3d88f63d64f912304b5d236f2a5e50
Amit <amit.yd525@gmail.com>
2024-03-29 19:18:46 +0530

    working on profile page of TA

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4 ">
+    <footer className="bg-gray-800 text-white text-center py-4" >
       {/* Footer content */}
       <p>&copy; 2024 Graduate Teaching Assistant Management System. All rights reserved.</p>
 
