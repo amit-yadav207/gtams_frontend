6ffda385eeaf09ecbefacd3da36fcb2b52697e82
Amit <amit.yd525@gmail.com>
2024-03-30 12:33:26 +0530

    jobcreation update

--- src/pages/DeptStaffPages/JobCreationForm.jsx
+++ src/pages/DeptStaffPages/JobCreationForm.jsx
@@ -156,6 +156,23 @@ const JobCreationForm = () => {
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
             />
           </div>
+          <div className="mb-4">
+            <label
+              htmlFor="jobId"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              jobId:
+            </label>
+            <input
+              type="text"
+              id="jobId"
+              name="jobId"
+              value={formData.jobId} // Convert array to comma-separated string
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+            />
+          </div>
 
           <div className="mb-4">
             <label className=" text-gray-700 font-semibold mb-2 flex justify-start items-center">
