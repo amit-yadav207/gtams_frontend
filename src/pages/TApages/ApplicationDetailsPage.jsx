a8ccfc4b4b49acfdc3ae881b75c3c53709697a0f
Amit <amit.yd525@gmail.com>
2024-04-03 22:50:43 +0530

    modified application detail page

--- src/pages/TApages/ApplicationDetailsPage.jsx
+++ src/pages/TApages/ApplicationDetailsPage.jsx
@@ -1,13 +1,30 @@
-// ApplicationDetailsPage.jsx
-
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../Helper/axiosInstance";
 import toast from "react-hot-toast";
+import {
+  FaUser,
+  FaBriefcase,
+  FaFileAlt,
+  FaCheckCircle,
+  FaCalendarAlt,
+} from "react-icons/fa";
+
+
+//date format
+const formatDate = (dateString) => {
+  const options = { day: 'numeric', month: 'short', year: 'numeric' };
+  return new Date(dateString).toLocaleDateString('en-US', options);
+};
+// Function to convert bytes to kilobytes
+const bytesToKB = (bytes) => {
+  return (bytes / 1024).toFixed(2); // Convert bytes to KB and round to 2 decimal places
+};
 
 const ApplicationDetailsPage = () => {
   const { jobId } = useParams(); // Get jobId from route parameters
   const [application, setApplication] = useState(null);
+  const [jobTitle, setJobTitle] = useState("");
 
   useEffect(() => {
     const getApplicationDetails = async () => {
@@ -27,6 +44,7 @@ const ApplicationDetailsPage = () => {
         console.log("data", res.data);
 
         setApplication(res.data.form);
+        setJobTitle((await res).data.jobTitle);
       } catch (error) {
         toast.error("Failed to fetch application details");
       }
@@ -40,49 +58,145 @@ const ApplicationDetailsPage = () => {
   }
 
   return (
-    <div className="m-8 sm:m-2">
-      <h1 className="text-3xl font-bold px-5 py-3 text-gray-800">
+    <div className="md:m-4 m-2">
+      <h1 className="text-lg md:text-3xl font-bold px-5 py-3 text-gray-800">
         Application Details
       </h1>
-      <div className="m-6 p-4 rounded-xl border-gray-300 shadow-md">
-        <p className="text-lg font-semibold">Applicant Name:</p>
-        <p className="text-gray-700">{application.applicantName}</p>
-
-        <p className="text-lg font-semibold mt-4">Department:</p>
-        <p className="text-gray-700">{application.department}</p>
-
-        <p className="text-lg font-semibold mt-4">Course ID:</p>
-        <p className="text-gray-700">{application.courseId}</p>
-
-        <p className="text-lg font-semibold mt-4">Email:</p>
-        <p className="text-gray-700">{application.email}</p>
-
-        <p className="text-lg font-semibold mt-4">Previous Experience:</p>
-        {application.previousExperience.map((exp, index) => (
-          <div key={index} className="mt-2">
-            <p className="text-gray-700">{exp.course}</p>
-            <p className="text-gray-700">From: {exp.fromDate}</p>
-            <p className="text-gray-700">To: {exp.toDate}</p>
+      <div className="p-3 md:p-6">
+        {/* Personal Details */}
+        <div className="md:mb-4 mb-1 shadow-md rounded-lg p-4 md:p-6  ">
+          <div className="flex items-center mb-3">
+            <FaUser className="mr-2" size={20} />
+            <p className="lg:text-2xl text-lg font-semibold">
+              Personal Details
+            </p>
           </div>
-        ))}
 
-        <p className="text-lg font-semibold mt-4">Resume:</p>
-        <p className="text-gray-700">
-          File Name: {application.resume.fileName}
-        </p>
-        <p className="text-gray-700">Size: {application.resume.size}</p>
+          <table className="w-full md:text-lg text-base">
+            <tbody>
+              <tr className="mb-2 sm:mb-1">
+                <td className="font-semibold">Applicant Name:</td>
+                <td>{application.applicantName}</td>
+              </tr>
+              <tr className="mb-2 sm:mb-1">
+                <td className="font-semibold">Email:</td>
+                <td>{application.email}</td>
+              </tr>
+              <tr className="mb-2 sm:mb-1">
+                <td className="font-semibold">Gender:</td>
+                <td>{application.gender}</td>
+              </tr>
+              <tr className="mb-2 sm:mb-1">
+                <td className="font-semibold">Contact:</td>
+                <td>{application.phone}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
-        <p className="text-lg font-semibold mt-4">Status:</p>
-        <p
-          className={`text-lg ${
-            application.status === "Pending" ? "font-bold" : ""
-          }`}
-        >
-          {application.status}
-        </p>
+        {/* Applied Job Details */}
+        <div className="md:mb-2 mb-1 shadow-md rounded-lg p-4 md:p-6">
+          <div className="flex items-center mb-3">
+            <FaBriefcase className="mr-2" size={20} />
+            <p className="lg:text-2xl text-lg font-semibold">
+              Applied Job Details
+            </p>
+          </div>
 
-        <p className="text-lg font-semibold mt-4">Applied Date:</p>
-        
+          <table className="w-full md:text-lg text-base">
+            <tbody>
+              <tr className="mb-2 sm:mb-1">
+                <td className="font-semibold">Job ID:</td>
+                <td>{jobId}</td>
+              </tr>
+              <tr className="mb-2 sm:mb-1">
+                <td className="font-semibold">Job Title:</td>
+                <td>{jobTitle}</td>
+              </tr>
+              <tr className="mb-2 sm:mb-1">
+                <td className="font-semibold">Department:</td>
+                <td>{application.department}</td>
+              </tr>
+              <tr className="mb-2 sm:mb-1">
+                <td className="font-semibold">Course ID:</td>
+                <td>{application.courseId}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        {/* Previous Experience */}
+        <div className="md:mb-2 mb-1 shadow-md rounded-lg p-4 md:p-6">
+          <div className="flex items-center mb-3">
+            <FaBriefcase className="mr-2" size={20} />
+            <p className="lg:text-2xl text-lg font-semibold">
+              Previous Experience
+            </p>
+          </div>
+
+          {application.previousExperience.length > 0 ? (
+            <table className="w-full md:text-lg text-base">
+              <tbody>
+                <tr className="mb-2 sm:mb-1">
+                  <td className="font-semibold">Course Name</td>
+                  <td className="font-semibold">From</td>
+                  <td className="font-semibold">To</td>
+                </tr>
+
+                {application.previousExperience.map((exp, index) => (
+                  <tr key={index} className="mb-1 sm:mb-0">
+                    <td >{exp.course}</td>
+                    <td >{formatDate(exp.fromDate)}</td>
+                    <td >{formatDate(exp.toDate)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p className="font-semibold">No previous experience.</p>
+          )}
+        </div>
+
+        {/* Resume */}
+        <div className=" mb-2 md:mb-4   shadow-md rounded-lg p-4 md:p-6">
+          <div className="flex items-center mb-3">
+            <FaFileAlt className="mr-2" size={20} />
+            <p className="lg:text-2xl text-lg font-semibold">Resume</p>
+          </div>
+
+          <table className="w-full md:text-lg text-base">
+            <tbody>
+              <tr className="md:mb-2 mb-1">
+                <td className="font-semibold">File Name:</td>
+                <td>{application.resume.fileName}</td>
+              </tr>
+              <tr className="md:mb-2 mb-1">
+                <td className="font-semibold">Size:</td>
+                <td>{bytesToKB(application.resume.size)} KB</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        {/* Status and Applied Date */}
+        <div className="md:mb-4 mb-2  shadow-md rounded-lg p-4 md:p-6">
+          <div className="flex items-center mb-3">
+            <FaCheckCircle className="mr-2" size={20} />
+            <p className="lg:text-2xl text-lg font-semibold">Status</p>
+          </div>
+          <p
+            className={`text-lg sm:text-base ${
+              application.status === "Pending" ? "font-bold" : ""
+            }`}
+          >
+            {application.status}
+          </p>
+          <div className="flex items-center mt-3">
+            <FaCalendarAlt className="mr-2" size={20} />
+            <p className="lg:text-2xl text-lg font-semibold">Applied Date</p>
+          </div>
+          {/* Add your applied date here */}
+        </div>
       </div>
     </div>
   );
